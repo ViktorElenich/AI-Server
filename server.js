ca6c3893627eca2e05f20251b7734179fce67f94
viktorelenich <velenic@mail.ru>
2026-01-18 21:09:18 +0300

    fix: ai model

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ app.post('/api/chat', async (req, res) => {
                     content: message
                 }
             ],
-            model: "llama3-8b-8192", 
+            model: "llama-3.3-70b-versatile", 
             stream: true,
         });
 
